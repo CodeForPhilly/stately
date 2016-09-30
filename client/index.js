3b0148fa8ce24f26c78f9b38e84ad02277e068a0
timwis <tim@timwis.com>
2016-09-30 06:14:07 -0400

    add choo-log in development mode

--- client/index.js
+++ client/index.js
@@ -5,6 +5,11 @@ const CaseView = require('./views/case')
 
 const app = choo()
 
+if (process.env.NODE_ENV !== 'production') {
+  const log = require('choo-log')
+  app.use(log())
+}
+
 app.model(require('./models/case'))
 
 app.router((route) => [
